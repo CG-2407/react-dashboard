678a402ac915754c58d712be86545b5f884a357f
CG-2407 <62751408+CG-2407@users.noreply.github.com>
2023-02-01 14:40:20 -0500

    theme colors and dark mode

--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -10,9 +10,27 @@ const initialState = {
 }
 
 export const ContextProvider = ({children}) => {
-    const[activeMenu, setActiveMenu] = useState(true);
+    const [activeMenu, setActiveMenu] = useState(true);
     const [isClicked, setisClicked] = useState(initialState);
     const [screenSize, setScreenSize] = useState(undefined);
+    const [currentColor, setCurrentColor] = useState('#03C9D7');
+    const [currentMode, setCurrentMode] = useState('Light');
+    const [themeSettings, setThemeSettings] = useState(false);
+
+    const setMode = (e) => {
+        setCurrentMode(e.target.value);
+
+        localStorage.setItem('themeMode', e.target.value);
+        setThemeSettings(false);
+    }
+    const setColor = (color) => {
+        setCurrentColor(color);
+
+        localStorage.setItem('colorMode', color);
+        setThemeSettings(false);
+
+    }
+    
     const handleClick = (clicked) => {
         setisClicked({...initialState, [clicked]:true});
     }
@@ -26,6 +44,10 @@ export const ContextProvider = ({children}) => {
                 handleClick,
                 screenSize,
                 setScreenSize,
+                currentColor, currentMode,
+                setCurrentColor, setCurrentMode,
+                themeSettings, setThemeSettings,
+                setMode, setColor
             }}
         >
             {children}
--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -7,6 +7,9 @@ import { useStateContext } from '../context/ContextProvider';
 
 
 const Ecommerce = () => {
+
+  const { currentColor } = useStateContext(); 
+
   return (
     <div className='mt-12'>
       <div className='flex flex-wrap lg:flex-nowrap justify-center'>
@@ -21,7 +24,7 @@ const Ecommerce = () => {
           <div className='mt-'>
             <Button
               color="white"
-              bgColor="blue"
+              bgColor={currentColor}
               text="Download"
               borderRadius="10px"
               size="md"
@@ -101,19 +104,19 @@ const Ecommerce = () => {
                 <p className='text-gray-500 mt-1'>Expense</p>
               </div>
               <div className='mt-5'>
-                <SparkLine currentColor="blue"
+                <SparkLine currentColor={currentColor}
                   id="line-sparkline"
                   type="Line"
                   height="80px"
                   width="250px"
                   data={SparklineAreaData}
-                  color="blue"
+                  color={currentColor}
                 />
               </div>
               <div className='mt-10'>
                 <Button
                   color='white'
-                  bgColor='blue'
+                  bgColor={currentColor}
                   text="Download Report"
                   borderRadius='10px'
                 />
